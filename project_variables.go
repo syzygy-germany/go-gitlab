3f5beebd75a18ce73fb168069207d1cc55eefd90
Sascha Andres <sascha.andres@outlook.com>
2018-05-18 15:11:41 +0200

    feat: add create project variable

--- project_variables.go
+++ project_variables.go
@@ -41,3 +41,39 @@ func (s *ProjectVariablesService) ListVariables(pid interface{}, opt *ListProjec
 
 	return pv, resp, err
 }
+
+// CreateProjectVariableOptions represents the available CreateVariable() options.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/project_level_variables.html#create-variable
+type CreateProjectVariableOptions struct {
+	Key       *string `url:"key,omitempty" json:"key,omitempty"`
+	Value     *string `url:"value,omitempty" json:"value,omitempty"`
+	Protected *bool   `url:"protected,omitempty" json:"protected,omitempty"`
+}
+
+// CreateVariable creates a new project snippet. The user must have permission
+// to create new snippets.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/project_level_variables.html#create-variable
+func (s *ProjectVariablesService) CreateVariable(pid interface{}, opt *CreateProjectVariableOptions, options ...OptionFunc) (*Variable, *Response, error) {
+	project, err := parseID(pid)
+	if err != nil {
+		return nil, nil, err
+	}
+	u := fmt.Sprintf("projects/%s/variables", url.QueryEscape(project))
+
+	req, err := s.client.NewRequest("POST", u, opt, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pv := new(Variable)
+	resp, err := s.client.Do(req, pv)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return pv, resp, err
+}
